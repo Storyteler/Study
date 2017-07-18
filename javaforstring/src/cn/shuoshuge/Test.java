89cba76169400eb888c25904224f4a13f620b4cf
Storyteler <18864770547@163.com>
2017-07-18 22:16:43 +0800

    关于String类的方法使用

---
+++ javaforstring/src/cn/shuoshuge/Test.java
@@ -0,0 +1,73 @@
+package cn.shuoshuge;
+
+/*
+ * 此类为String类的一些方法调用
+*/
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		Integer in = 1344;
+		String str = String.valueOf(in);
+		System.out.println(str);
+		
+//		String str = Integer.toString(1);
+//		System.out.println();
+		
+		
+//		String str = "       accd   a1    ";
+//		System.out.println(str.trim());
+		
+		
+//		System.out.println(str.toUpperCase().toLowerCase());
+//		System.out.println(str);
+		
+/*		String[] part = str.split("a");
+		for(String string : part) {
+			System.out.println(string);
+		}*/
+		
+//		System.out.println(str.replaceFirst("a","b"));
+		
+//		System.out.println(str.contains("a"));
+		
+//		System.out.println(str.replace(" ",">"));
+		
+//		System.out.println(str.concat("sjdk"));
+		
+//		System.out.println(str.substring(1,4));
+		
+//		System.out.println(str.substring(2));
+		
+//		String str2 = "abcd";
+		
+		
+//		System.out.println(str.lastIndexOf('a'));
+//		System.out.println(str.indexOf("a"));
+		
+//		System.out.println(str.indexOf('1'));
+		
+//		System.out.println(str.startsWith("a",1));
+		
+//		System.out.println(str.compareTo(str2));
+		
+//		System.out.println(str.equalsIgnoreCase(str2));
+		
+//		System.out.println(str.codePointBefore(1));
+		
+//		System.out.println(str.codePointAt(1));
+		
+//		System.out.println(str.charAt(1));
+		
+		
+		//将字符数组转化为字符串
+/*		char[] nums = {'a','b','c'};
+		String str = new String(nums);
+		
+		System.out.println(str);*/
+		
+	}
+
+}
