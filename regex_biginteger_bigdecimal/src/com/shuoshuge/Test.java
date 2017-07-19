00b94f571a89a7ad7ac9fe88a52758ee7d7bd5df
Storyteler <18864770547@163.com>
2017-07-19 17:08:01 +0800

    完成正则表达式的应用与BigInteger类和BigDecimal类的使用

---
+++ regex_biginteger_bigdecimal/src/com/shuoshuge/Test.java
@@ -0,0 +1,33 @@
+package com.shuoshuge;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//BigDecimal类的使用
+/*		BigDecimal bd1 = new BigDecimal("9");
+		BigDecimal bd2 = new BigDecimal("4");
+		System.out.println(bd1.divide(bd2));
+		System.out.println(bd1.divide(bd2, RoundingMode.UP));
+		System.out.println(bd1.divide(bd2, RoundingMode.DOWN));*/
+		
+		
+		//BigInteger类的使用
+/*		BigInteger bi1 = new BigInteger("74075643828237");
+		BigInteger bi2 = new BigInteger("23");
+		System.out.println(bi1.multiply(bi2));*/
+		
+
+		//对邮箱进行正则表达式验证
+/*		String str = "18864770547@163.com";
+		String regex = "\\w+@\\w+(\\.\\w+){1,2}";
+		
+		System.out.println(str.matches(regex));*/
+		
+	}
+
+}
